af3fe382b956f6fad81553dbf2aca9b297922942
神楽坂雅詩 <cyscxchope@gmail.com>
2016-01-07 14:43:17 +0800

    创建工程

---
+++ YashiCascadeString_CS/YashiCascadeString_CS/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YashiCascadeString_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "Yashi Cascade String Data Format";
+            
+        }
+    }
+}
